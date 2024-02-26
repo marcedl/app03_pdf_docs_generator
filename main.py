1aa57be0e825299e881314c428d8a00126f90ae2
Marcela DL <leon.ari00@hotmail.com>
2024-02-26 11:11:40 +0100

    adding rows in each page

--- main.py
+++ main.py
@@ -12,8 +12,9 @@ for index, row in df.iterrows():
     pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
+    for i in range(20, 290, 10):
+        pdf.line(10, i, 200, i)
     pdf.line(10, 20, 200, 20)
-
     #Set footer only on first page
     pdf.ln(265)
 
@@ -24,6 +25,8 @@ for index, row in df.iterrows():
 
     for i in range(row["Pages"] - 1):
         pdf.add_page()
+        for i in range(20, 290, 10):
+            pdf.line(10, i, 200, i)
         #Set footer for other pages
         pdf.ln(277)
         pdf.set_font(family="Times", style="I", size=8)
